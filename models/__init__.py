837b2d8206b218db53f328ac94935911fb548457
mazhf <85665689+mazhf@users.noreply.github.com>
2022-05-23 19:35:20 +0800

    Add files via upload

---
+++ models/__init__.py
@@ -0,0 +1,13 @@
+from .ms_convlstm import Multi_Scale_ConvLSTM
+from .ms_trajgru import Multi_Scale_TrajGRU
+from .ms_predrnn import Multi_Scale_PredRNN
+from .ms_predrnn_plus2 import Multi_Scale_PredRNN_plus2
+from .ms_mim import Multi_Scale_MIM
+from .ms_motionrnn_mim import Multi_Scale_MotionRNN_MIM
+
+from .mm_convlstm import Multi_Modal_ConvLSTM
+from .mm_trajgru import Multi_Modal_TrajGRU
+from .mm_predrnn import Multi_Modal_PredRNN
+from .mm_predrnn_plus2 import Multi_Modal_PredRNN_plus2
+from .mm_mim import Multi_Modal_MIM
+from .mm_motionrnn_mim import Multi_Modal_MotionRNN_MIM
---
+++ models/mm_motionrnn_mim.py
@@ -0,0 +1,143 @@
+from torch import nn
+import torch
+import sys
+sys.path.append("..")
+from config import cfg
+from models.ms_motionrnn_mim import MotionRNN_cell
+from util.attention import MAM
+
+
+class Multi_Modal_MotionRNN_MIM(nn.Module):
+    def __init__(self, input_channel, output_channel, b_h_w, kernel_size, stride, padding):
+        super().__init__()
+        self.n_layers = cfg.LSTM_layers
+        B, H, W = b_h_w
+        lstm = [MotionRNN_cell(input_channel, output_channel, [B, H, W], kernel_size, stride, padding),
+                MotionRNN_cell(input_channel, output_channel, [B, H // 2, W // 2], kernel_size, stride, padding),
+                MotionRNN_cell(input_channel, output_channel, [B, H // 4, W // 4], kernel_size, stride, padding),
+                MotionRNN_cell(input_channel, output_channel, [B, H // 4, W // 4], kernel_size, stride, padding),
+                MotionRNN_cell(input_channel, output_channel, [B, H // 2, W // 2], kernel_size, stride, padding),
+                MotionRNN_cell(input_channel, output_channel, [B, H, W], kernel_size, stride, padding)]
+        self.lstm = nn.ModuleList(lstm)
+
+        lstm_ele = [MotionRNN_cell(input_channel, output_channel, [B, H, W], kernel_size, stride, padding),
+                    MotionRNN_cell(input_channel, output_channel, [B, H // 2, W // 2], kernel_size, stride, padding),
+                    MotionRNN_cell(input_channel, output_channel, [B, H // 4, W // 4], kernel_size, stride, padding),
+                    MotionRNN_cell(input_channel, output_channel, [B, H // 4, W // 4], kernel_size, stride, padding),
+                    MotionRNN_cell(input_channel, output_channel, [B, H // 2, W // 2], kernel_size, stride, padding),
+                    MotionRNN_cell(input_channel, output_channel, [B, H, W], kernel_size, stride, padding)]
+        self.lstm_ele = nn.ModuleList(lstm_ele)
+
+        self.downs = nn.ModuleList([nn.MaxPool2d(2, 2), nn.MaxPool2d(2, 2)])
+        self.downs_ele = nn.ModuleList([nn.MaxPool2d(2, 2), nn.MaxPool2d(2, 2)])
+
+        self.ups = nn.ModuleList([nn.Upsample(scale_factor=2, mode='bilinear'), nn.Upsample(scale_factor=2, mode='bilinear')])
+        self.ups_ele = nn.ModuleList([nn.Upsample(scale_factor=2, mode='bilinear'), nn.Upsample(scale_factor=2, mode='bilinear')])
+
+        self.downs_m = nn.ModuleList([nn.MaxPool2d(2, 2), nn.MaxPool2d(2, 2)])
+        self.downs_m_ele = nn.ModuleList([nn.MaxPool2d(2, 2), nn.MaxPool2d(2, 2)])
+
+        self.ups_m = nn.ModuleList([nn.Upsample(scale_factor=2, mode='bilinear'), nn.Upsample(scale_factor=2, mode='bilinear')])
+        self.ups_m_ele = nn.ModuleList([nn.Upsample(scale_factor=2, mode='bilinear'), nn.Upsample(scale_factor=2, mode='bilinear')])
+
+        self.downs_xt_1 = nn.ModuleList([nn.MaxPool2d(2, 2), nn.MaxPool2d(2, 2)])
+        self.downs_xt_1_ele = nn.ModuleList([nn.MaxPool2d(2, 2), nn.MaxPool2d(2, 2)])
+
+        self.ups_xt_1 = nn.ModuleList([nn.Upsample(scale_factor=2, mode='bilinear'), nn.Upsample(scale_factor=2, mode='bilinear')])
+        self.ups_xt_1_ele = nn.ModuleList([nn.Upsample(scale_factor=2, mode='bilinear'), nn.Upsample(scale_factor=2, mode='bilinear')])
+
+        if cfg.use_Attention:
+            Att = [MAM(input_channel, H, W, 5), MAM(input_channel, H // 2, W // 2, 4), MAM(input_channel, H // 4, W // 4, 3)]
+            self.Att = nn.ModuleList(Att)
+
+        print('This is Multi Modal MotionRNN-MIM!')
+
+    def forward(self, x_all, m_all, layer_hiddens, embed, embed_ele, fc, fc_ele):  # b8hw
+        x = x_all[:, 0, ...]  # bhw
+        x = torch.unsqueeze(x, dim=1)  # b1hw
+        x_ele = x_all[:, 1:, ...]  # b7hw
+
+        x = embed(x)  # bchw
+        x_ele = embed_ele(x_ele)  # bchw
+
+        m, m_ele = m_all
+
+        next_layer_hiddens = []
+        out = []
+        out_ele = []
+        for l in range(self.n_layers):
+            if layer_hiddens is not None:
+                hiddens = layer_hiddens[l][0]
+                xt_1 = layer_hiddens[l - 1][0][0]
+                hiddens_ele = layer_hiddens[l][1]
+                xt_1_ele = layer_hiddens[l - 1][1][0]
+                if l == 0:
+                    xt_1 = None
+                    xt_1_ele = None
+                elif l == 1:
+                    xt_1 = self.downs_xt_1[0](xt_1)
+                    xt_1_ele = self.downs_xt_1_ele[0](xt_1_ele)
+                elif l == 2:
+                    xt_1 = self.downs_xt_1[1](xt_1)
+                    xt_1_ele = self.downs_xt_1_ele[1](xt_1_ele)
+                elif l == 4:
+                    xt_1 = self.ups_xt_1[0](xt_1)
+                    xt_1_ele = self.ups_xt_1_ele[0](xt_1_ele)
+                elif l == 5:
+                    xt_1 = self.ups_xt_1[1](xt_1)
+                    xt_1_ele = self.ups_xt_1_ele[1](xt_1_ele)
+            else:
+                hiddens = None
+                hiddens_ele = None
+                xt_1 = None
+                xt_1_ele = None
+            # elements
+            x_ele, m_ele, next_hiddens_ele = self.lstm_ele[l](x_ele, xt_1_ele, m_ele, hiddens_ele, l)
+            out_ele.append(x_ele)
+
+            # radar elements fuse
+            if cfg.use_Attention:
+                if l == 3:
+                    x = self.Att[2](x, x_ele)
+                elif l == 4:
+                    x = self.Att[1](x, x_ele)
+                elif l == 5:
+                    x = self.Att[0](x, x_ele)
+            else:
+                if l == 3:
+                    x = x + x_ele
+                elif l == 4:
+                    x = x + x_ele
+                elif l == 5:
+                    x = x + x_ele
+
+            # radar
+            x, m, next_hiddens = self.lstm[l](x, xt_1, m, hiddens, l)
+            out.append(x)
+
+            if l == 0:
+                x = self.downs[0](x)
+                x_ele = self.downs_ele[0](x_ele)
+                m = self.downs_m[0](m)
+                m_ele = self.downs_m_ele[0](m_ele)
+            elif l == 1:
+                x = self.downs[1](x)
+                x_ele = self.downs_ele[1](x_ele)
+                m = self.downs_m[1](m)
+                m_ele = self.downs_m_ele[1](m_ele)
+            elif l == 3:
+                x = self.ups[1](x) + out[1]
+                x_ele = self.ups_ele[1](x_ele) + out_ele[1]
+                m = self.ups_m[1](m)
+                m_ele = self.ups_m_ele[1](m_ele)
+            elif l == 4:
+                x = self.ups[0](x) + out[0]
+                x_ele = self.ups_ele[0](x_ele) + out_ele[0]
+                m = self.ups_m[0](m)
+                m_ele = self.ups_m_ele[0](m_ele)
+
+            next_layer_hiddens.append([next_hiddens, next_hiddens_ele])
+
+        x = fc(x)  # b1hw
+        x_ele = fc_ele(x_ele)  # b7hw
+        return torch.cat([x, x_ele], dim=1), [m, m_ele], next_layer_hiddens
